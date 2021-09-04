8a936fb504f0ecfe1b3df6faafa368db40337b32
Rishitpate <90095284+Rishitpate@users.noreply.github.com>
2021-09-04 12:46:20 -0400

    Update SearchPage.java

--- src/test/java/DemoTest1/SearchPage.java
+++ src/test/java/DemoTest1/SearchPage.java
@@ -5,5 +5,9 @@ public class SearchPage {
 	public void Searchpage() {
 		System.out.println("SearchPage Method");
 	}
+	
+	public void Searchpage2() {
+		System.out.println("SearchPage Method 2");
+	}
 
 }
